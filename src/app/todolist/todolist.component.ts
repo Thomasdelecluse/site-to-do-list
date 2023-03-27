e9dde4aa6bb61193b009cde8e8cd45360a0a09da
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-03-28 00:33:23 +0200

    feat: add todolist form

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {ToDoListFormService} from "../../service/ToDoListFormService";
+import {HttpClient} from "@angular/common/http";
+import {ToDoListDAO} from "../../dao/ToDoListDAO";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-todolist',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodolistComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(public todoListController: ToDoListDAO, private router: Router) {
+  }
+
+  todoListForm = new FormGroup({
+    title: new FormControl(''),
+    type: new FormControl(''),
+    description: new FormControl(''),
+  })
 
   ngOnInit(): void {
   }
 
+  onFormSubmit() {
+    const todo = {
+      title: this.todoListForm.controls['title'].value ?? '',
+      type: this.todoListForm.controls['type'].value ?? '',
+      description: this.todoListForm.controls['description'].value ?? ''
+    }
+
+    this.todoListController.createToDo(todo).subscribe(() => {
+      // Rediriger vers la page d'accueil après avoir créé le todo
+      this.router.navigate(['/home']);
+    })
+
+  }
 }
