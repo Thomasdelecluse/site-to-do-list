b87cdec97f8dc1684e4cae2505dcbf0e703ded62
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-03-28 00:29:48 +0200

    feat: add deleteTodo, updateStatus, updateTableWithDatas

---
+++ src/service/ToDoListFormService.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "@angular/core";
+import {ToDoListDAO} from "../dao/ToDoListDAO";
+import {ToDoDTO} from "../app/dto/ToDoDTO";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToDoListFormService {
+
+  constructor(private todoListController: ToDoListDAO) {  }
+
+  public form(todo: {title: string, type: string, description: string}) {
+    this.todoListController.createToDo(todo)
+  }
+
+  public updateTableWithDatas() {
+    return this.todoListController.getAllToDo()
+  }
+
+  public updateStatus(id: number, todo: Omit<ToDoDTO, "id">) {
+   return  this.todoListController.putUpdateStatus(id,todo)
+  }
+
+  public deleteTodo(id: number) {
+    return  this.todoListController.deleteTodo(id)
+  }
+}
