e2929488901773148727a6f443ea990fd146f6c8
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-03-28 00:34:16 +0200

    feat: use back routes

---
+++ src/dao/ToDoListDAO.ts
@@ -0,0 +1,30 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {ToDoDTO} from "../app/dto/ToDoDTO";
+import {Observable} from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToDoListDAO {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  createToDo(todo: { title: string, type: string, description: string }) {
+    return this.httpClient.post('http://localhost:8080/todos', todo)
+  }
+
+  getAllToDo() : Observable<ToDoDTO[]> {
+    return this.httpClient.get<ToDoDTO[]>('http://localhost:8080/todos')
+  }
+
+  putUpdateStatus(id: number, todo: Omit<ToDoDTO, "id">) {
+    return this.httpClient.put('http://localhost:8080/todos/' + id, todo)
+  }
+
+  deleteTodo(id: number) {
+    return this.httpClient.delete('http://localhost:8080/todos/' + id)
+  }
+}
