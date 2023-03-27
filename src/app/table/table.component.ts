a72f97b4cffb68fd4cc09b6f61696ce12dd4ce86
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-03-28 00:32:03 +0200

    feat: create table component

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {ToDoListFormService} from "../../service/ToDoListFormService";
+import {ToDoDTO} from "../dto/ToDoDTO";
+import {Router} from "@angular/router";
+import {MatTableDataSource} from "@angular/material/table";
+
+
+const ELEMENT_DATA: ToDoDTO[] = [];
+
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'title', 'description', 'type', 'status', 'delete'];
+  dataSource: MatTableDataSource<ToDoDTO> = new MatTableDataSource<ToDoDTO>();
+
+  constructor(private todolistformService: ToDoListFormService) { }
+
+  ngOnInit(): void {
+    this.updateTable();
+
+  }
+
+  updateStatus(todo: ToDoDTO) {
+    this.todolistformService.updateStatus(todo.id, {
+      title: todo.title,
+      description: todo.description,
+      type: todo.type,
+      status: !todo.status,
+    }).subscribe(
+      () => {
+        this.updateTable();
+      }
+    );
+  }
+
+  deleteTodo(id : number) {
+    this.todolistformService.deleteTodo(id).subscribe(
+      () => {
+        this.updateTable();
+      }
+    );
+  }
+
+  updateTable() {
+    this.todolistformService.updateTableWithDatas().subscribe((response) => {
+      this.dataSource = new MatTableDataSource(response);
+    })
+  }
+}
