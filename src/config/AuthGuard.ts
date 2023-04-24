56c6d6699a44b4fc26fb5f125a8fce8c268bbbfd
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-04-24 21:58:22 +0200

    feat: AuthGuard upgrade for login user

--- src/config/AuthGuard.ts
+++ src/config/AuthGuard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +8,19 @@ export class AuthGuard implements CanActivate {
 
   constructor(private router: Router) {}
 
-  canActivate(): boolean {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const token = localStorage.getItem('token');
-    if (!token) {
+
+    if (!token && state.url !== '/login' && state.url !== '/register') {
       this.router.navigate(['/login']);
       return false;
     }
+
+    if (token && (state.url === '/login' || state.url === '/register')) {
+      this.router.navigate(['/home']);
+      return false;
+    }
+
     return true;
   }
 
