a04a166d87cd4289f994dac6a19d343fe5043366
ThomasDelecluseRedlab <81568452+Thomasdelecluse@users.noreply.github.com>
2023-04-24 15:12:24 +0200

    feat: use input type angular materials password for register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,6 +11,7 @@ import {Router} from "@angular/router";
 export class RegisterComponent implements OnInit {
 
   constructor(private authService: AuthService, private router: Router) { }
+  hide = true;
   error: string | undefined;
   ngOnInit(): void {
   }
